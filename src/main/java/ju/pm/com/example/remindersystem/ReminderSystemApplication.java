588ac9b41da167547c9739d4e8d54cb6491459ed
AkramJaghoub <akram.jaghoub55@gmail.com>
2024-01-06 17:48:58 +0300

    postgres

--- src/main/java/ju/pm/com/example/remindersystem/ReminderSystemApplication.java
+++ src/main/java/ju/pm/com/example/remindersystem/ReminderSystemApplication.java
@@ -2,10 +2,12 @@ package ju.pm.com.example.remindersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableJpaRepositories
 public class ReminderSystemApplication {
 
 	public static void main(String[] args) {
